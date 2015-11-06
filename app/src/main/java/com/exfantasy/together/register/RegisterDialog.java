ff20eb6308705f689bcaff5141eb509ffa7e7f98
yuanyu90221 <yuanyu90221@gmail.com>
2015-11-06 18:21:05 +0800

    adjust Content for register

--- app/src/main/java/com/exfantasy/together/register/RegisterDialog.java
+++ app/src/main/java/com/exfantasy/together/register/RegisterDialog.java
@@ -1,11 +1,12 @@
 package com.exfantasy.together.register;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
+
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
