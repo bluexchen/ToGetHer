11afe27f80ae806e00f7e62b7bd6e1f99a8a1ccd
TommyYehCool <tommy.yeh1112@gmail.com>
2015-11-17 17:04:19 +0800

    Optimize Imports

--- app/src/main/java/com/exfantasy/together/login/LoginDialog.java
+++ app/src/main/java/com/exfantasy/together/login/LoginDialog.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
