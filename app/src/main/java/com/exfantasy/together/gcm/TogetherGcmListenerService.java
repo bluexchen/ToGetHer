68ad449afe811017a56543634d9fb058e6df9ca2
TommyYehCool <tommy.yeh1112@gmail.com>
2015-12-02 17:35:50 +0800

    Refactoring

--- app/src/main/java/com/exfantasy/together/gcm/TogetherGcmListenerService.java
+++ app/src/main/java/com/exfantasy/together/gcm/TogetherGcmListenerService.java
@@ -19,7 +19,7 @@ import com.google.android.gms.gcm.GcmListenerService;
 
 public class TogetherGcmListenerService extends GcmListenerService {
 
-    private static final String TAG = "TogetherGcmListenerService";
+    private static final String TAG = "GcmListener";
 
     /**
      * Called when message is received.
