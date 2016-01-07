cd024d38cc438a3e159f7c05a1eb696844f87772
TommyYehCool <tommy.yeh1112@gmail.com>
2016-01-07 14:54:52 +0800

    Refactoring

--- app/src/main/java/com/exfantasy/together/register/RegisterDialog.java
+++ app/src/main/java/com/exfantasy/together/register/RegisterDialog.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.exfantasy.together.R;
